fe7a00f0e10fbda6f19ed5fa953160f397b6a502
Venisha Ambasana <venishaambasana@gmail.com>
2026-08-31 11:25:34 +0530

    Lab 10 Solutions

--- LAB 10/A_3.c
+++ LAB 10/A_3.c
@@ -1,30 +1,35 @@
 // 3.  Find whether the given number is prime or not. 
 
 #include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int num, i, isPrime;
-    
+int main()
+{
+    int n, i;
+
     printf("Enter a number: ");
-    scanf("%d", &num);
-    
-    isPrime = 1;  // Assume prime
-    i = 2;
-    
-    // Check divisibility from 2 to num-1
-    while(i < num){
-        if(num % i == 0){
-            isPrime = 0;  // Not prime
-            break;
+    scanf("%d", &n);
+
+    if (n <= 1)
+    {
+        printf("Not Prime");
+    }
+    else
+    {
+        i = 2;
+        while (i < n)
+        {   
+            // if we find a factor, terminate the program.
+            if (n % i == 0)
+            {
+                printf("Not Prime");
+                return 0;
+            }
+            i++;
         }
-        i = i + 1;
+
+        printf("Prime");
     }
-    
-    if(isPrime == 1){
-        printf("%d is Prime\n", num);
-    } else {
-        printf("%d is Not Prime\n", num);
-    }
-    
+
     return 0;
 }
